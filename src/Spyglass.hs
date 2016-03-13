9586935d195724746cfd10d0f5bcc7afa6240c54
Diogo Biazus <diogob@gmail.com>
2016-03-12 21:53:41 -0500

    Changes the types to more accurately describe the PostgreSQL AST, simplifythings and started encoding the update syntax.

--- src/Spyglass.hs
+++ src/Spyglass.hs
@@ -9,9 +9,8 @@ module Spyglass
     , whereClause
     , relationAlias
     , relationExpression
-    , qualifier
+    , exp
     , expression
-    , identifier
     , alias
     , fromClause
     , t
@@ -19,115 +18,144 @@ module Spyglass
     , setWhere
     ) where
 
-import Control.Lens
-import Data.String (IsString, fromString)
-import qualified Data.Text as T
-import TextShow (TextShow, showt, showb, fromText)
-import Data.Monoid ((<>))
+import           Control.Lens
+import           Data.List.NonEmpty (NonEmpty(..), toList)
+import           Data.Monoid        ((<>))
+import           Data.String        (IsString, fromString)
+import qualified Data.Text          as T
+import           TextShow           (TextShow, fromText, showb, showt)
 
-withCommas :: (TextShow a) => [a] -> T.Text
-withCommas showables = T.intercalate ", " $ map showt showables
+withCommas :: TextShow a => [a] -> T.Text
+withCommas s = T.intercalate ", " $ fmap showt s
 
-type Name = T.Text
-type Operator = T.Text
+newtype Operator = Operator T.Text deriving (Eq, Show)
+newtype Literal = Literal T.Text deriving (Eq, Show)
+newtype Name = Name T.Text deriving (Eq, Show)
 
-data Expression = Identifier Name
-                | QualifiedIdentifier
-                  { _qualifier :: Name
-                  , _identifier :: Name
-                  }
-                | Boolean Bool
-                | Or (Expression, Expression)
-                | And (Expression, Expression)
-                | Not Expression
-                | BinaryOperator (Expression, Operator, Expression)
-                | FunctionCall (Name, [Expression])
-                  deriving (Eq)
+data Identifier = Identifier
+               { _qualifier  :: Name
+               , _identifier :: Name
+               } deriving (Eq, Show)
 
-data Column = Column
+data BooleanExpression = Or (Expression, Expression)
+                       | And (Expression, Expression)
+                       | Not Expression
+                       | BoolLiteral Bool
+                       deriving (Eq, Show)
+
+data Expression = IdentifierExp Identifier
+                | BoolExp BooleanExpression
+                | OperatorExp (Expression, Operator, Expression)
+                | FunctionExp (Name, [Expression])
+                | QueryExp Select
+                | LiteralExp Literal
+                | NameExp Name
+                deriving (Eq, Show)
+
+data Column = Column Expression
+            | AliasedColumn
               { _expression :: Expression
-              , _alias :: Name
-              }
-            | ColumnExpression Expression
+              , _alias      :: Name
+              } deriving (Eq, Show)
 
-data FromItem = Relation
+data FromRelation = FromRelation
               { _relationExpression :: Expression
-              , _relationAlias :: Name
-              }
-            | RelationExpression Expression
+              , _relationAlias      :: Name
+              } deriving (Eq, Show)
 
 data Select = Select
-              { _columns :: [Column]
-              , _fromClause :: [FromItem]
+              { _columns     :: NonEmpty Column
+              , _fromClause  :: [FromRelation]
               , _whereClause :: Maybe Expression
-              }
+              } deriving (Eq, Show)
 
+data Assignment = Assignment
+                  { _targetColumn :: Name
+                  , _assignmentValue :: Expression
+                  } deriving (Eq, Show)
+
+data Update = Update
+              { _targetRelation       :: Identifier
+              , _assignments  :: NonEmpty Assignment
+              , _updateFilter :: Maybe Expression
+              } deriving (Eq, Show)
+
+makeLenses ''Update
 makeLenses ''Select
-makeLenses ''FromItem
+makeLenses ''FromRelation
 makeLenses ''Column
 makeLenses ''Expression
 
-instance TextShow Expression where
-  showb (Boolean True) = "true"
-  showb (Boolean False) = "false"
-  showb (Identifier "*") = "*"
-  showb (Identifier i) = showb i
-  showb (QualifiedIdentifier q i) = showb q <> "." <> showb i
+instance TextShow BooleanExpression where
   showb (Or (e1, e2)) = showb e1 <> " OR " <> showb e2
   showb (And (e1, e2)) = showb e1 <> " AND " <> showb e2
   showb (Not e) = "NOT " <> showb e
-  showb (BinaryOperator (e1, operator, e2)) = showb e1 <> " " <> fromText operator <> " " <> showb e2
-  showb (FunctionCall (i, parameters)) = showb i <> "(" <> fromText (withCommas parameters) <> ")"
+  showb (BoolLiteral True) = "true"
+  showb (BoolLiteral False) = "false"
 
-instance TextShow FromItem where
-  showb (Relation c a) = showb c
-                      <> " AS "
-                      <> showb a
-  showb (RelationExpression e) = showb e
+instance TextShow Identifier where
+  showb (Identifier e1 e2) = showb e1 <> "." <> showb e2
+
+instance TextShow Literal where
+  showb (Literal e) = showb e
+
+instance TextShow Expression where
+  showb (IdentifierExp e) = showb e
+  showb (BoolExp e) = showb e
+  showb (OperatorExp (e1, Operator operator, e2)) = showb e1 <> " " <> fromText operator <> " " <> showb e2
+  showb (FunctionExp (i, parameters)) = showb i <> "(" <> fromText (withCommas parameters) <> ")"
+  showb (QueryExp e) = showb e
+  showb (LiteralExp e) = showb e
+  showb (NameExp e) = showb e
+
+instance TextShow FromRelation where
+  showb (FromRelation e a) = showb e <> " " <> showb a
+
+instance TextShow Name where
+  showb (Name "*") = "*"
+  showb (Name c) = showb c
 
 instance TextShow Column where
-  showb (Column c a) = showb c
-                      <> " AS "
-                      <> showb a
-  showb (ColumnExpression e) = showb e
+  showb (Column c) = showb c
+  showb (AliasedColumn c a) = showb c <> " AS " <> showb a
 
 instance TextShow Select where
-  showb (Select c f w) =
-    select <> case w of
+  showb (Select c fr w) =
+    sel <> case w of
         Just ex -> " WHERE " <> showb ex
         Nothing -> ""
-    where select = "SELECT " <> fromText (withCommas c) <>
-                   if null f
+    where sel = "SELECT " <> fromText (withCommas $ toList c) <>
+                   if null fr
                       then ""
                       else " FROM "
-                    <> fromText (withCommas f)
+                    <> fromText (withCommas fr)
 
-instance IsString Expression where
-  fromString = Identifier . fromString
-
-instance IsString FromItem where
-  fromString = RelationExpression . fromString
+instance IsString Name where
+  fromString = Name . fromString
 
 instance IsString Column where
-  fromString = ColumnExpression . fromString
+  fromString = Column . NameExp . fromString
+
+instance IsString FromRelation where
+  fromString e = FromRelation (NameExp $ fromString e) (fromString e)
 
 select :: Select
-select = Select ["*"] [] Nothing
+select = Select ("*" :| []) [] Nothing
 
-selectFrom :: Expression -> Select
-selectFrom table = select & fromClause .~ [RelationExpression table]
+selectFrom :: Name -> Select
+selectFrom table = select & fromClause .~ [FromRelation (NameExp table) table]
 
 selectExp :: Expression -> Select
-selectExp expr = select & columns .~ [ColumnExpression expr]
+selectExp expr = select & columns .~ (Column expr :| [])
 
 selectFunction :: T.Text -> [Expression] -> Select
-selectFunction fn parameters = selectExp $ FunctionCall (fn, parameters)
+selectFunction fn parameters = selectExp $ FunctionExp (Name fn, parameters)
 
 t :: Expression
-t = Boolean True
+t = BoolExp $ BoolLiteral True
 
 f :: Expression
-f = Boolean False
+f = BoolExp $ BoolLiteral False
 
 setWhere :: Expression -> Select -> Select
 setWhere = set whereClause . Just
